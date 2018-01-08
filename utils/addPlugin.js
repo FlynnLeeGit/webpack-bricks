ce08e5d23d1eb37515d101e3a0e3a5f4e7cd3851
FlynnLee <593029372@qq.com>
2018-01-08 13:35:09 +0800

    addPlugins fn

--- utils/addPlugin.js
+++ utils/addPlugin.js
@@ -1,7 +1,12 @@
 const merge = require('webpack-merge')
 
-module.exports = plugin => config => {
+module.exports = arg => config => {
+  if (Array.isArray(arg)) {
+    return merge(config, {
+      plugins: [...arg]
+    })
+  }
   return merge(config, {
-    plugins: [plugin]
+    plugins: [arg]
   })
 }
